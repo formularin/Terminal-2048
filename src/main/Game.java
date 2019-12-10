b1b2b684ff7389e2a0301a02f577add12860c670
lol-cubes <arinmkhare@gmail.com>
2019-12-09 21:12:26 -0500

    figure out how imports and stuff will work

---
+++ src/main/Game.java
@@ -0,0 +1,18 @@
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.bundle.LanternaThemes;
+import com.googlecode.lanterna.gui2.BasicWindow;
+import com.googlecode.lanterna.gui2.DefaultWindowManager;
+import com.googlecode.lanterna.gui2.EmptySpace;
+import com.googlecode.lanterna.gui2.Label;
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("This is a game.");
+    }
+}
