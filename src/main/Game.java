85c3e4436ea9d4a027462c3e4d5558fde1feba22
lol-cubes <arinmkhare@gmail.com>
2019-12-11 22:30:54 -0500

    create simple program to display 2x2 grid of octothorpes

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,18 +1,32 @@
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.bundle.LanternaThemes;
-import com.googlecode.lanterna.gui2.BasicWindow;
-import com.googlecode.lanterna.gui2.DefaultWindowManager;
-import com.googlecode.lanterna.gui2.EmptySpace;
-import com.googlecode.lanterna.gui2.Label;
-import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.graphics.BasicTextImage;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+
+import java.io.IOException;
 
 
 public class Game {
-    public static void main(String[] args) {
-        System.out.println("This is a game.");
+    public static void main(String[] args) throws IOException, InterruptedException {
+            
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+        screen.startScreen();
+
+        TextGraphics graphics = screen.newTextGraphics();
+
+        TerminalSize terminal_size = new TerminalSize(2, 2);
+        TextCharacter text_character = new TextCharacter('#');
+        BasicTextImage image = new BasicTextImage(terminal_size, text_character);
+
+        graphics.drawImage(new TerminalPosition(0, 0), image);
+        screen.refresh();
+
+        Thread.sleep(5000);
     }
 }
