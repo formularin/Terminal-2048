f7808e28a32e6465baeda0cac3c2eab9c8a7b6ee
lol-cubes <arinmkhare@gmail.com>
2019-12-15 11:03:15 -0500

    create graphics package with canvas class and test with simple animation

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,34 +7,70 @@ import com.googlecode.lanterna.graphics.BasicTextImage;
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.VirtualScreen.FrameRenderer;
 
 import java.io.IOException;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import graphics.Canvas;
+
 
 public class Game {
+
     public static void main(String[] args) throws IOException, InterruptedException {
-        
+        // create small animation to test graphics capabilities
+
+        Logger logger = Game.createLoggger();
+
         Terminal terminal = new DefaultTerminalFactory().createTerminal();
         Screen stdscr = new TerminalScreen(terminal);
         stdscr.startScreen();
 
+        TerminalSize terminal_size = terminal.getTerminalSize();
         TextGraphics graphics = stdscr.newTextGraphics();
-        String screen_image = "";  // What is to be displayed on the screen
+        
+        Canvas canvas = new Canvas(10, 10);
 
-        for (int f = 0; f < 500; f++) {
-            // main loop
+        for (int r = 0; r < canvas.rows; r++) {
+            for (int c = 0; c < canvas.columns; c++) {
+                // update virtual screen
 
-            // update virtual screen
-            screen_image = Integer.toString(f);
+                // replace previous "#" with " "
+                if (c == 0) {
+                    if (r != 0) {
+                        canvas.replace(r - 1, canvas.columns - 1, " ");
+                    }
+                } else {
+                    canvas.replace(r, c - 1, " ");
+                }
 
-            // update real screen
-            stdscr.clear();
-            graphics.putString(new TerminalPosition(0, 0), screen_image);
-            stdscr.refresh();
+                // place new "#"
+                canvas.replace(r, c, "#");
 
-            Thread.sleep(10);
+                // update real screen
+                stdscr.clear();
+                String[] canvas_rows = canvas.get_rows();
+                for ( int i = 0; i < canvas.grid.length; i++) {
+                    graphics.putString(new TerminalPosition(0, i), canvas_rows[i]);
+                }
+                stdscr.refresh();
+
+                Thread.sleep(100);
+            }
         }
 
         stdscr.close();
+    }
+
+    private static Logger createLoggger() throws IOException {
+        FileHandler handler = new FileHandler("terminal-2048.log", true);
+        Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        logger.addHandler(handler);
+        logger.setUseParentHandlers(false);
+
+        return logger;
     }
 }
---
+++ src/main/java/graphics/Canvas.java
@@ -0,0 +1,27 @@
+package graphics;
+
+public class Canvas {
+    public int rows;
+    public int columns;
+    public String[][] grid;
+
+    public Canvas(int r, int c) {
+        rows = r;
+        columns = c;
+        grid = new String[rows][columns];
+    }
+
+    public void replace(int r, int c, String ch) {
+        grid[r][c] = ch;
+    }
+
+    public String[] get_rows() {
+
+        String[] row_list = new String[rows];
+        for (int r = 0; r < grid.length; r++) {
+            row_list[r] = String.join("", grid[r]).replace("null", " ");
+        }
+
+        return row_list;
+    }
+}
